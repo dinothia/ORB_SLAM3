214e843ab56434945239f25797ecd39a9f62f105
dinossht <dinosshan1996@hotmail.com>
2021-01-26 10:58:55 +0100

    copy same as fro Install/ORB-SLAM3 on school pc

--- build_ros.sh
+++ build_ros.sh
@@ -3,5 +3,5 @@ echo "Building ROS nodes"
 cd Examples/ROS/ORB_SLAM3
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3
+cmake .. -DROS_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 -L/usr/local/lib/libopencv_imgproc.so.3.2
 make -j
